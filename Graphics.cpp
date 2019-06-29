fd9f40709f49a09b12942fc4c0ca35a18a9250e2
dkudrin <dkudrin@gmail.com>
2019-06-29 18:05:35 +0300

    Add more Error info - Lesson 14

--- Graphics.cpp
+++ Graphics.cpp
@@ -5,8 +5,19 @@
 #pragma comment(lib, "d3d11.lib")
 
 
-#define GFX_THROW_FAILED(hrcall) if (FAILED(hr = (hrcall))) throw Graphics::HrException(__LINE__, __FILE__, hr)
+// graphics exception checking/throwing macros (some with dxgi infos)
+#define GFX_EXCEPT_NOINFO(hr) Graphics::HrException( __LINE__,__FILE__,(hr) )
+#define GFX_THROW_NOINFO(hrcall) if( FAILED( hr = (hrcall) ) ) throw Graphics::HrException( __LINE__,__FILE__,hr )
+
+#ifndef NDEBUG
+#define GFX_EXCEPT(hr) Graphics::HrException( __LINE__,__FILE__,(hr),infoManager.GetMessages() )
+#define GFX_THROW_INFO(hrcall) infoManager.Set(); if( FAILED( hr = (hrcall) ) ) throw GFX_EXCEPT(hr)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr),infoManager.GetMessages() )
+#else
+#define GFX_EXCEPT(hr) Graphics::HrException( __LINE__,__FILE__,(hr) )
+#define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
 #define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException(__LINE__, __FILE__, (hr))
+#endif
 
 Graphics::Graphics(HWND hWnd)
 {
@@ -25,20 +36,25 @@ Graphics::Graphics(HWND hWnd)
 	sd.SampleDesc.Count = 1; // AntiAliasing - no AA for today
 	sd.SampleDesc.Quality = 0;
 	// etc
-	sd.OutputWindow = (HWND)6969696; // hWnd; // Дескриптор окна, в которое будет происходить отрисовка
+	sd.OutputWindow = hWnd; // Дескриптор окна, в которое будет происходить отрисовка
 	sd.Windowed = TRUE;
 	sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD; // Выключение swap еффекта - выключаем, это обеспечивает лучший вид и производительность
 	sd.Flags = 0;
+
+	UINT swapCreateFlags = 0u;
+#ifndef NDEBUG
+	swapCreateFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
 
 	// for checkong results of d3d functions
 	HRESULT hr;
 
 	// create device and front/back duffers, and swap chain and rednering context
-	GFX_THROW_FAILED(D3D11CreateDeviceAndSwapChain(
+	GFX_THROW_INFO(D3D11CreateDeviceAndSwapChain(
 		nullptr, // using default video card
 		D3D_DRIVER_TYPE_HARDWARE, // DriverType - hardware driver
 		nullptr, // pp - handle to the Software driver, if we want use it
-		D3D11_CREATE_DEVICE_DEBUG, // Flags D3D11_CREATE_DEVICE_DEBUG - get debug info in std::out
+		swapCreateFlags, // Flags D3D11_CREATE_DEVICE_DEBUG - get debug info in std::out
 		nullptr,  //  Feature Levels - default
 		0, // Feature Levels
 		D3D11_SDK_VERSION, // SDK Version
@@ -50,13 +66,13 @@ Graphics::Graphics(HWND hWnd)
 	));
 	// gain access to texture subresource in swap chain (back buffer)
 	ID3D11Resource* pBackBuffer = nullptr;
-	GFX_THROW_FAILED(pSwap->GetBuffer(
+	GFX_THROW_INFO(pSwap->GetBuffer(
 		0, // index of the buffer, that we want to get. 0 - backbuffer
 		__uuidof(ID3D11Resource), // uuid of the interface that we want to recieve
 		reinterpret_cast<void**>(&pBackBuffer) // pp that we can use to fill our pointer
 	));
 	// once we have a handler to device recource we can use it to create render target view on it
-	GFX_THROW_FAILED(pDevice->CreateRenderTargetView(
+	GFX_THROW_INFO(pDevice->CreateRenderTargetView(
 		pBackBuffer, // backbuffer
 		nullptr, // additional options
 		&pTarget // pp to our target
@@ -87,6 +103,9 @@ Graphics::~Graphics()
 void Graphics::EndFrame()
 {
 	HRESULT hr;
+#ifndef NDEBUG
+	infoManager.Set();
+#endif
 	if (FAILED(hr =
 		pSwap->Present( // Показ первого экрана из swap-chain
 			1u, // Если вам кажется что ваша игра не может проигрываться плавно на 60fps то можно поставить здесь коэф 2
@@ -99,7 +118,7 @@ void Graphics::EndFrame()
 		}
 		else
 		{
-			GFX_THROW_FAILED(hr);
+			throw GFX_EXCEPT(hr);
 		}
 	}
 }
@@ -112,11 +131,23 @@ void Graphics::ClearBuffer(float red, float green, float blue)
 
 
 // Graphics exception stuff
-Graphics::HrException::HrException(int line, const char * file, HRESULT hr)
+Graphics::HrException::HrException(int line, const char * file, HRESULT hr, std::vector<std::string> infoMsgs)
 	:
 	Exception(line, file),
 	hr(hr)
-{}
+{
+	// join all info messages with newlines into single string
+	for (const auto& m : infoMsgs)
+	{
+		info += m;
+		info.push_back('\n');
+	}
+	// remove final newline if exists
+	if (!info.empty())
+	{
+		info.pop_back();
+	}
+}
 
 const char * Graphics::HrException::what() const
 {
@@ -125,8 +156,13 @@ const char * Graphics::HrException::what() const
 		<< "[Error Code] 0x" << std::hex << std::uppercase << GetErrorCode()
 		<< std::dec << " (" << (unsigned long)GetErrorCode() << ")" << std::endl
 		<< "[Error String] " << GetErrorString() << std::endl
-		<< "[Description] " << GetErrorDescription() << std::endl
-		<< GetOriginString();
+		<< "[Description] " << GetErrorDescription() << std::endl;
+	if (!info.empty())
+	{
+		oss << "\n[Error Info]\n" << GetErrorInfo() << std::endl << std::endl;
+	}
+	oss << GetOriginString();
+
 	whatBuffer = oss.str();
 	return whatBuffer.c_str();
 }
@@ -152,6 +188,12 @@ std::string Graphics::HrException::GetErrorDescription() const
 	DXGetErrorDescription(hr, buf, sizeof(buf));
 	return buf;
 }
+
+std::string Graphics::HrException::GetErrorInfo() const
+{
+	return info;
+}
+
 
 const char* Graphics::DeviceRemovedException::GetType() const
 {
--- Graphics.h
+++ Graphics.h
@@ -2,6 +2,8 @@
 #include "ChiliWin.h"
 #include "ChiliException.h"
 #include <d3d11.h>
+#include <vector>
+#include "DxgiInfoManager.h"
 
 class Graphics
 {
@@ -13,20 +15,24 @@ public:
 	class HrException : public Exception // graphic exception that have HRESULT
 	{
 	public:
-		HrException(int line, const char * file, HRESULT hr);
+		HrException(int line, const char * file, HRESULT hr, std::vector<std::string> infoMsgs = {});
 		const char * what() const override;
 		const char * GetType() const override;
 		HRESULT GetErrorCode() const;
 		std::string GetErrorString() const;
 		std::string GetErrorDescription() const;
+		std::string GetErrorInfo() const;
 	private:
 		HRESULT hr;
+		std::string info;
 	};
 	class DeviceRemovedException : public HrException
 	{
 		using HrException::HrException;
 	public:
 		const char* GetType() const override;
+	private:
+		std::string reason;
 	};
 public:
 	Graphics(HWND hWnd);
@@ -36,6 +42,9 @@ public:
 	void EndFrame();
 	void ClearBuffer(float red, float green, float blue); // function for clearing buffer with color
 private:
+#ifndef NDEBUG
+	DxgiInfoManager infoManager;
+#endif
 	ID3D11Device* pDevice = nullptr;
 	IDXGISwapChain* pSwap = nullptr;
 	ID3D11DeviceContext* pContext = nullptr;
